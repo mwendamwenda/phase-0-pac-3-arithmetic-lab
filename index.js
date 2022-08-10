82377826dec3f13c4b9c1956a45f985a860a6feb
mwendamwenda <benmwenda83@gmail.com>
2022-08-10 14:12:58 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,64 @@
+function add(){
+    return (1 + 80);
+}
+add();
+ 
+function subtract(){
+    return(60 - 40);
+}
+subtract();
+
+function multiply(){
+    return(2 * 3.4);
+}
+multiply();
+
+function divide() {
+    return(5 / 2.5)
+}
+divide();
+
+function add(a, b){
+     return(a +b)
+    }
+    add(1+180)
+
+
+function subtract(a, b) {
+    return(a - b)
+}
+subtract(60, 40)
+
+function multiply(a,b){
+    return(a * b)
+}
+multiply(a, b)
+
+function divide(a, b){
+    return(a/ b)
+}
+divide(5 ,2.5)
+
+function increment(n) {
+    n++
+    return n;
+}
+increment(n)
+
+function decrement(n) {
+    n--
+    return n;
+}
+decrement(n)
+
+function makeInt(n) {
+
+    return parseInt(n, 10);
+
+}
+makeInt(n)
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+    
+}
